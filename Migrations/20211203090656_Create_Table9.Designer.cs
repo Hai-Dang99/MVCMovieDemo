3feed326557e44062d6cecf0641471455932f3b0
Hai-Dang99 <haidang250899@gmail.com>
2021-12-03 16:21:35 +0700

    2

--- Migrations/20211203090656_Create_Table9.Designer.cs
+++ Migrations/20211203090656_Create_Table9.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MVCMovie.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCMovie.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211203090656_Create_Table9")]
+    partial class Create_Table9
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Migrations/20211203091704_Create_Table11.Designer.cs
+++ Migrations/20211203091704_Create_Table11.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MVCMovie.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCMovie.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211203091704_Create_Table11")]
+    partial class Create_Table11
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
