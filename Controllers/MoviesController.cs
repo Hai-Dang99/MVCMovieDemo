23cd1985619a4975d17644e4dbdaa3dbe35783e0
Hai-Dang99 <haidang250899@gmail.com>
2021-12-10 16:30:55 +0700

    2

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +9,17 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVCMovie.Models;
 using MVCMovie.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
 
 namespace MVCMovie.Controllers
 {
     public class MoviesController : Controller
     {
         private readonly ApplicationDBContext _context;
-
+        ExcelProcess _excelPro= new ExcelProcess();
+        private WriteDataToDatabse _wdtDatabase = new WriteDataToDatabse ();
         public MoviesController(ApplicationDBContext context)
         {
             _context = context;
@@ -78,32 +84,52 @@ namespace MVCMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie exam, IFormFile file)
         {
+    if (file!=null)
+    {
+        string fileExtension = Path.GetExtension(file.FileName);
+        if (fileExtension != ".xls" && fileExtension != ".xlsx")
+        {
+            ModelState.AddModelError("", "Please choose excel file to upload!");
+        }
+        else
+        {
+            //rename file when upload to server
+            //tao duong dan /Uploads/Excels de luu file upload len server
+            var fileName = "Ten file muon luu";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", fileName + fileExtension);
+            var fileLocation = new FileInfo(filePath).ToString();
+
             if (ModelState.IsValid)
             {
-                _context.Add(movie);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                //upload file to server
+                if (file.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //save file to server
+                        await file.CopyToAsync(stream);
+                        //read data from file and write to database
+                        //_excelPro la doi tuong xu ly file excel ExcelProcess
+                        var dt = _excelPro.ExcelToDataTable(fileLocation);
+                        //ghi du lieu datatable vao database
+                        if (exam.Subject==0)
+                        {
+                            WriteMovie(dt);
+                        }
+                        else
+                        {
+                            WriteEnglishMovie(dt);
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
             }
-            return View(movie);
         }
-
-        // GET: Movies/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var movie = await _context.Movie.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-            return View(movie);
-        }
+    }
+    return View(exam);
+}
 
         // POST: Movies/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -174,4 +200,23 @@ namespace MVCMovie.Controllers
             return _context.Movie.Any(e => e.Id == id);
         }
     }
+    private int WriteMovie(DataTable dt)
+    {
+        try{
+            var con = Configuration.GetConnectionString("ApplicationContext");
+            SqlBulkCopy bulkCopy = new SqlBulkCopy(con);
+            bulkCopy.DestinationTableName = "Movie";
+            bulkCopy.ColumnMappings.Add(1, "Title");
+            bulkCopy.ColumnMappings.Add(2,"Genre");
+            bulkCopy.ColumnMappings.Add(3,"Rating");
+            bulkCopy.WriteToSever(dt);
+
+        }
+        catch
+        {
+            return 0;
+        }
+    
+    }
 }
+
